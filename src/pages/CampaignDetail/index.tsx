4d367c3faf04d8a573e8ddf4057ec4efb85df0bd
rafabruno <brunor_morais@hotmail.com>
2020-11-05 11:17:26 -0300

    get param id

--- src/pages/CampaignDetail/index.tsx
+++ src/pages/CampaignDetail/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+interface ParamTypes {
+  id: string;
+}
 
 const CampaignDetail: React.FC = () => {
+  const { id } = useParams<ParamTypes>();
+
   return (
     <div>
-      <span>campaign detail</span>
+      <span>
+        campaign detail
+        {id}
+      </span>
     </div>
   );
 };
--- src/pages/NewAction/index.tsx
+++ src/pages/NewAction/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+interface ParamTypes {
+  id: string;
+}
 
 const NewAction: React.FC = () => {
+  const { id } = useParams<ParamTypes>();
+
   return (
     <div>
-      <span>new action</span>
+      <span>
+        new action for id:
+        {id}
+      </span>
     </div>
   );
 };
