c70c03736ddf577db146d104d98422d6424fdfcf
rafabruno <brunor_morais@hotmail.com>
2020-11-04 22:09:45 -0300

    add normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Routes from './routes';
+import './styles/normalize.css';
 
+import Routes from './routes';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
