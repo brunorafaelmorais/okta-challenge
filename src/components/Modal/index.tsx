4dbc81be7db221dc3c39ffd9ec6a5e751dc35f07
rafabruno <brunor_morais@hotmail.com>
2020-11-09 15:06:47 -0300

    create modals

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import ReactModal from 'react-modal';
+
+export interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ children, isOpen, setIsOpen }) => {
+  const [modalStatus, setModalStatus] = useState(isOpen);
+
+  useEffect(() => {
+    setModalStatus(isOpen);
+  }, [isOpen]);
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          transform: 'translate(-50%, -50%)',
+          border: 0,
+          padding: 0,
+          width: 992,
+          maxWidth: 'calc(100% - 32px)',
+          maxHeight: 'calc(100% - 32px)',
+          display: 'flex',
+          flexDirection: 'column',
+        },
+        overlay: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
